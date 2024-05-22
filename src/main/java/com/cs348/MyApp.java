58bb73e62f59a8e7e9d6bfe74c88efeb6ed65b1d
harry446 <yxliu@uwaterloo.ca>
2024-05-22 18:59:46 -0400

    added test program and updated gitignore

--- src/main/java/com/cs348/MyApp.java
+++ src/main/java/com/cs348/MyApp.java
@@ -3,5 +3,9 @@ package src.main.java.com.cs348;
 public class MyApp {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        for (int i=0; i<10; i++) {
+            System.out.println(i);
+        }
     }
 }
