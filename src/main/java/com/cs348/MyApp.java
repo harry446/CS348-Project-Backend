8e3e2177a8d9d8c4c616cfaed6b388507eb72542
kevinli20040106 <z66li@uwaterloo.ca>
2024-05-22 20:19:50 -0400

    Connection to data base source code

--- src/main/java/com/cs348/MyApp.java
+++ src/main/java/com/cs348/MyApp.java
@@ -1,11 +1,20 @@
 package src.main.java.com.cs348;
+import java.sql.*;
 
 public class MyApp {
     public static void main(String[] args) {
         System.out.println("hello world");
 
-        for (int i=0; i<10; i++) {
-            System.out.println(i);
+        String url = "jdbc:mysql://localhost:3306/sonoo";
+        String username = "root";
+        String password = "Kw14101106";
+
+        System.out.println("Connecting database ...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
         }
         hello();
     }
