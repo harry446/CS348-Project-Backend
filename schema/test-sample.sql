0d5ce016408c970678cdc8fe4f2acdfe24e6f400
lilywangtn <lilywangtn@gmail.com>
2024-07-07 18:44:27 -0400

    Add files via upload

--- schema/test-sample.sql
+++ schema/test-sample.sql
@@ -35,5 +35,4 @@ ORDER BY temp.price DESC;
 -- feature 5: liking a parking lot
 UPDATE lots SET like_num = like_num+1 WHERE lid = 2 AND lid IN 
 (SELECT lid FROM Bookings WHERE uid = 3 AND status = True AND end_time < CURRENT_TIMESTAMP AND liked = False);
-UPDATE bookings SET liked = True WHERE bid = 3 AND status = True AND end_time < CURRENT_TIMESTAMP;
-
+UPDATE bookings SET liked = True WHERE uid = 3 AND lid = 2 AND status = True AND end_time < CURRENT_TIMESTAMP;
