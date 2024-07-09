c44168a6d6007ab1b504cc0a48388c2967ced983
ZG-GC <84283702+ZG-GC@users.noreply.github.com>
2024-07-09 17:19:25 -0400

    Update test-production.sql

--- schema/test-production.sql
+++ schema/test-production.sql
@@ -27,5 +27,5 @@ UPDATE lots SET like_num = like_num+1 WHERE lid = 4 AND lid IN
 (SELECT lid FROM Bookings WHERE uid = 20 AND status = True AND end_time < CURRENT_TIMESTAMP AND liked = False);
 UPDATE bookings SET liked = True WHERE uid = 20 AND lid = 4 AND status = True AND end_time < CURRENT_TIMESTAMP;
 
-SELECT COUNT(*) FROM users WHERE username ='brittanyoneill' AND password = 'eI%5#@Igp';
+SELECT COUNT(*) FROM users WHERE username = ‘bellrobert’ AND password = 'S&#dA15tkgMLnx#a';
 
