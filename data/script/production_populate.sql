d754d47f8d6be46ae1cafc770d9a0bd1ea78a8f9
ZG-GC <84283702+ZG-GC@users.noreply.github.com>
2024-07-24 16:06:33 -0400

    Update production_populate.sql

--- data/script/production_populate.sql
+++ data/script/production_populate.sql
@@ -23,3 +23,10 @@ FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 (lid,sid,parking_type,latitude,longitude,max_stay,price)
 SET max_stay = NULLIF(max_stay, 'NULL');
+
+LOAD DATA LOCAL INFILE '/path/to/your/allspots.txt'
+INTO TABLE bookings
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+(bid, uid, lid, sid, create_time, start_time, end_time, price, status, liked)
+SET price = NULLIF(price, 'NULL');
